00a36886d9e03ac62d536c32a238ed2b2bfedc5b
Taulant Ismailgeci <+>
2022-04-07 18:07:13 -0400

    updated all of the subfolders and pages of the main website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+/* stuff to be added here later
 
-document.querySelector("body").appendChild(h2);
+
+*/
